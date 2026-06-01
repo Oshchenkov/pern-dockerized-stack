d7a340d1411436c739229249d0b1e68b5ccc844c
v.oshchenkov <oshchenkovv@gmail.com>
2026-06-01 14:29:01 +0200

    added global catch errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ErrorWrapper } from "./error-wrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Header />
-        {children}
+        <ErrorWrapper>{children}</ErrorWrapper>
       </body>
     </html>
   );
