44f5f90727d061e46726cea920081c9c92dfa03d
v.oshchenkov <oshchenkovv@gmail.com>
2026-06-18 12:59:08 +0000

    add prettier in devcontainer

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,11 +1,17 @@
 import type { User } from "@/types/common.types";
-import { greet } from "@repo/shared-types"
+import { greet } from "@repo/shared-types";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
-        Welcome to Next.js! {greet('Test shared2')}
+        Welcome to Next.js!
       </h1>
+      {greet && (
+        <p className="mt-4 text-lg text-gray-600 dark:text-gray-400">
+          {greet("Test shared2")}
+        </p>
+      )}
       <p className="mt-4 text-lg text-gray-600 dark:text-gray-400">
         This is a simple starter template to get you up and running quickly.
       </p>
