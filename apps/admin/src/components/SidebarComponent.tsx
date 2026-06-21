432b312a33bc2d9d27fa8957f4c899c65841774d
v.oshchenkov <oshchenkovv@gmail.com>
2026-06-21 21:20:30 +0200

    test data

--- apps/admin/src/components/SidebarComponent.tsx
+++ apps/admin/src/components/SidebarComponent.tsx
@@ -4,16 +4,42 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarMenuButton,
+  SidebarSeparator,
 } from "@/components/ui/sidebar";
 
 export default function SidebarComponent() {
   return (
     <Sidebar>
       <SidebarHeader />
+      <SidebarSeparator />
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
+        <SidebarGroup />
+        <SidebarGroup />
+        <SidebarMenuButton
+          render={
+            <a href={"#"}>
+              <span>fdgfg</span>
+            </a>
+          }
+        >
+          44
+        </SidebarMenuButton>
+        <SidebarMenuButton isActive>Btn</SidebarMenuButton>
+
+        <SidebarGroup />
+        <SidebarGroup />
+        <SidebarGroup />
+        <SidebarGroup />
+        <SidebarGroup />
+        <SidebarGroup />
+        <SidebarGroup />
+        <SidebarGroup />
+        <SidebarGroup />
       </SidebarContent>
+      <SidebarSeparator />
       <SidebarFooter />
     </Sidebar>
   );
