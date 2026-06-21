7930987e4906019465b53aefacd1e917f5e6d529
v.oshchenkov <oshchenkovv@gmail.com>
2026-06-21 17:43:18 +0200

    created layout and added sidebar

---
+++ apps/admin/src/routes/_mainLayout.tsx
@@ -0,0 +1,19 @@
+import SidebarComponent from "@/components/SidebarComponent";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export const Route = createFileRoute("/_mainLayout")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <SidebarProvider>
+      <SidebarComponent />
+      <main>
+        <SidebarTrigger />
+        <Outlet />
+      </main>
+    </SidebarProvider>
+  );
+}
--- apps/admin/src/routes/_mainLayout/index.tsx
+++ apps/admin/src/routes/_mainLayout/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Button, buttonVariants } from "@/components/ui/button";
 
-export const Route = createFileRoute("/")({ component: Home });
+export const Route = createFileRoute("/_mainLayout/")({ component: Home });
 
 function Home() {
   return (
