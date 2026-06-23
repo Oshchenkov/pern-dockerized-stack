1652307b6d0eab2652fb433b1c9538dd031ac294
v.oshchenkov <oshchenkovv@gmail.com>
2026-06-23 16:31:54 +0200

    added admin docker copmose service for development target

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
 
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
     tailwindcss(),
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
     viteReact(),
   ],
-})
+  server: {
+    host: true, // Tells Vite to listen on all addresses, including LAN and public IPs
+    port: 5000, // Ensure this matches your intended port
+  },
+});
 
-export default config
+export default config;
