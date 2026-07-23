6338c6387a1e3feb817db0024ea92828e71d1dd5
v.oshchenkov <oshchenkovv@gmail.com>
2026-07-23 09:45:13 +0000

    updated monorepo shared types, added zod

--- shared/packages/shared-types/src/index.ts
+++ shared/packages/shared-types/src/index.ts
@@ -1,7 +1,11 @@
 // packages/shared/src/index.ts
-export const greet = (name: string) => `Hello, ${name} from the shared package!`;
+export const greet = (name: string) =>
+  `Hello, ${name} from the shared package!`;
 
 export type User = {
   id: string;
   name: string;
 };
+
+export * from "./schemas/index.js";
+export * from "./types/index.js";
