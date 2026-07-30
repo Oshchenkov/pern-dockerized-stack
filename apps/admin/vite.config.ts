b4cc6e213cd2f3bf4c6ffdc2e3ad2a7001b71fb6
Vitalii Oshchenkov <oshchenkovv@gmail.com>
2026-07-30 14:31:42 +0000

    updeted admin vite config for use https

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
-
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "node:fs";
 
-const config = defineConfig({
+const TLS_CERT = "/certs/tls/tls.crt";
+const TLS_KEY = "/certs/tls/tls.key";
+
+const https =
+  fs.existsSync(TLS_CERT) && fs.existsSync(TLS_KEY)
+    ? {
+        cert: fs.readFileSync(TLS_CERT),
+        key: fs.readFileSync(TLS_KEY),
+      }
+    : undefined;
+
+export default defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
@@ -15,9 +25,15 @@ const config = defineConfig({
     viteReact(),
   ],
   server: {
-    host: true, // Tells Vite to listen on all addresses, including LAN and public IPs
-    port: 5000, // Ensure this matches your intended port
+    host: "0.0.0.0",
+    port: 5000,
+    https,
+    proxy: {
+      "/api": {
+        target: "http://api:4000",
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/api/, ""),
+      },
+    },
   },
 });
-
-export default config;
